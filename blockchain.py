6f5ee170e4dbfaf1ebe66bbf468e0c0cf3826294
Sumesh Chandran <sumchan@msn.com>
2025-09-24 21:07:45 -0700

    Section-4

--- blockchain.py
+++ blockchain.py
@@ -1,5 +1,13 @@
 # Initializing our (empty) blockchain list
-blockchain = []
+genesis_block ={
+    'previous_hash':'', 
+    'index':0, 
+    'transactions': []
+}
+blockchain = [genesis_block]
+open_transactions = []
+owner = 'Sumesh'
+
 
 
 def get_last_blockchain_value():
@@ -11,24 +19,34 @@ def get_last_blockchain_value():
 # This function accepts two arguments.
 # One required one (transaction_amount) and one optional one (last_transaction)
 # The optional one is optional because it has a default value => [1]
-def add_transaction(transaction_amount, last_transaction=[1]):
-    """ Append a new value as well as the last blockchain value to the blockchain.
-    
-    Arguments:
-        :transaction_amount: The amount that should be added.
-        :last_transaction: The last blockchain transaction (default [1]).
-    """
-    if last_transaction == None:
-        last_transaction = [1]
-    blockchain.append([last_transaction, transaction_amount])
+def add_transaction(recipient, sender=owner, amount=1.0):
+    transaction = {
+        'sender': sender,
+        'recipient': recipient,
+        'amount': amount}
+    open_transactions.append(transaction)
+   
+def mine_block():
+    last_block = blockchain[-1]
+    hashed_block = ''
+    for key in last_block:
+        value = last_block[key]
+        hashed_block = hashed_block + str(value)
 
+    print(hashed_block)
+    block = {
+        'previous_hash':'XYZ', 
+        'index':len(blockchain), 
+        'transactions': open_transactions
+    }
+    blockchain.append(block)
 
 def get_transaction_value():
     """ Returns the input of the user (a new transaction amount) as a float. """
     # Get the user input, transform it from a string to a float and store it in user_input
-    user_input = float(input('Your transaction amount please: '))
-    return user_input
-
+    tx_recipient = input('Enter the recipient of the transaction: ') 
+    tx_amount= float(input('Your transaction amount please: '))
+    return (tx_recipient, tx_amount)
 
 def get_user_choice():
     user_input=input('Your choice: ')
@@ -40,7 +58,6 @@ def print_blockchain_elements():
         print(block)
     else:
         print('-' * 20)
-
 
 def verify_chain():
     # block_index = 0
@@ -54,30 +71,26 @@ def verify_chain():
         else:
             is_valid = False
             break
-    # for block in blockchain:
-    #     if block_index == 0:
-    #         block_index += 1
-    #         continue
-    #     elif block[0] == blockchain[block_index - 1]:
-    #         is_valid = True
-    #     else:
-    #         is_valid = False
-    #         break
-    #     block_index += 1
+
     return is_valid
 
 waiting_for_input = True
 while waiting_for_input:
     print('Please choose: ')
     print('1: Add a new transaction value')
-    print('2: Output the blockchain blocks')
+    print('2: Mine a new block')
+    print('3: Output the blockchain blocks')
     print('h: Manipulate the chain')
     print('q: Quit')
     user_choice = get_user_choice()
     if user_choice == '1':
-        tx_amount = get_transaction_value()
-        add_transaction(tx_amount, get_last_blockchain_value())
+        tx_data = get_transaction_value()
+        recipient, amount = tx_data
+        add_transaction(recipient, amount=amount)
+        print(open_transactions)
     elif user_choice == '2':
+        mine_block()
+    elif user_choice == '3':
         print_blockchain_elements()
     elif user_choice == 'h':
         if len(blockchain) >= 1:
@@ -86,10 +99,10 @@ while waiting_for_input:
         waiting_for_input = False      
     else:
         print('Input was invalid, please pick a value from list!')
-    if not verify_chain():
-        print_blockchain_elements()
-        print('Invalid blockchain!')
-        break
+    # if not verify_chain():
+    #     print_blockchain_elements()
+    #     print('Invalid blockchain!')
+    #     break
 else:
     print('User left!')
 
